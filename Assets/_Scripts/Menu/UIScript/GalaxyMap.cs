16251ceb3a18eb525756443f5a34aff5c3112afb
ranjitjohn01 <38595565+ranjitjohn01@users.noreply.github.com>
2020-02-12 19:10:37 -0800

    galaxyMap locations

---
+++ Assets/_Scripts/Menu/UIScript/GalaxyMap.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GalaxyMap : MonoBehaviour
+{
+    public void GoToLocation1()
+    {
+        SceneManager.LoadScene("Encounter");
+    }
+}
