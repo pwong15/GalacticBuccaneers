0862ca4591570a21a89218affff8bd0bdf2ccc33
Ian Ward <golfinian@gmail.com>
2020-02-27 11:37:13 -0800

    Tiles

--- Assets/Utilities/BoardCreationUtility/Scripts/GridSquare.cs
+++ Assets/Utilities/BoardCreationUtility/Scripts/GridSquare.cs
@@ -5,63 +5,180 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 
-
-class GridSquare : MonoBehaviour
+public class GridSquare : MonoBehaviour
 {
-    private float xCoordinate, yCoordinate, zCoordinate;
+    private float xCoordf, yCoordf, zCoordf;
+    private int column, row, zCoord;
     Renderer rend;
     Board gameBoard;
-    bool isHighlighted = false;
+    List<GridSquare> _neighbors;
+    bool canMoveUp = true, canMoveDown = true, canMoveRght = true, canMoveLft = true, isWall = false;
+    //bool isHighlighted = false;
 
     void Start()
     {
         rend = GetComponent<Renderer>();
     }
 
+    void Update()
+    {
+        CheckForCursorHover();
+    }
+
     public void OnMouseDown()
     {
-        if (!isHighlighted)
+        List<GridSquare> adjacencies = GetNeighbors();
+        Debug.Log("Clicked: " + this.ToString());
+        foreach(GridSquare g in adjacencies)
         {
-            Highlight();
-            Debug.Log(this.xCoordinate + " " + this.yCoordinate);
-            int column = GridConverter.RoundXCoordToInt(xCoordinate);
-            int row = GridConverter.RoundYCoordToPosInt(yCoordinate);
+            Debug.Log("adjacency: " + g.ToString());
+        }
+        //if (!isHighlighted)
+        //{
+        //    Highlight();
+        //    Debug.Log(this.xCoordinate + " " + this.yCoordinate);
+        //    int column = GridConverter.RoundXCoordToInt(xCoordinate);
+        //    int row = GridConverter.RoundYCoordToPosInt(yCoordinate);
 
-            gameBoard.MarkSquareAsWall(column, row);
+        //    gameBoard.MarkSquareAsWall(column, row);
+        //}
+        //else
+        //{
+        //    UnHighlight();
+        //    Debug.Log("Unmarked " + this.xCoordinate + " " + this.yCoordinate);
+        //    int column = GridConverter.RoundXCoordToInt(xCoordinate);
+        //    int row = GridConverter.RoundYCoordToPosInt(yCoordinate);
+
+        //    gameBoard.UnMarkSquareAsWall(column, row);
+        //}
+    }
+
+    //// Changes the tile color to green
+    //public void Highlight()
+    //{
+    //    rend.material.color = Color.green;
+    //    isHighlighted = true;
+    //}
+
+    //public void UnHighlight()
+    //{
+    //    isHighlighted = false;
+    //    rend.material.color = Color.white;
+    //}
+
+    // Creates an empty tile, sets its location, adds a box collider
+    public void Initialize(Board gameBoard, int xLocation, int yLocation, int zLocation, char layoutSymbol)
+    {
+        this.xCoordf = xLocation;
+        this.yCoordf = yLocation;
+        this.zCoordf = zLocation;
+
+        this.column = xLocation;
+        this.row = -yLocation;
+
+        this.gameBoard = gameBoard;
+        this.gameObject.AddComponent(typeof(BoxCollider));
+        this.transform.position = new Vector3(xCoordf, yCoordf, zCoordf);
+        GetComponent<Renderer>().enabled = !GetComponent<Renderer>().enabled;
+        LocateWalls(layoutSymbol);
+    }
+
+    // Checks if the cursor is hovering on this tile
+    private void CheckForCursorHover()
+    {
+        Vector3 cursorLocation = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        bool cursorIsOnTile = CursorIsOnTile(cursorLocation.x - xCoordf, cursorLocation.y - yCoordf);
+
+        // If cursor is inside the tile: highlight the tile
+        if (cursorIsOnTile && !isWall)
+        {
+            GetComponent<Renderer>().enabled = true; 
         }
         else
         {
-            UnHighlight();
-            Debug.Log("Unmarked " + this.xCoordinate + " " + this.yCoordinate);
-            int column = GridConverter.RoundXCoordToInt(xCoordinate);
-            int row = GridConverter.RoundYCoordToPosInt(yCoordinate);
-
-            gameBoard.UnMarkSquareAsWall(column, row);
+            GetComponent<Renderer>().enabled = false;
         }
     }
 
-    // Changes the tile color to green
-    public void Highlight()
+    
+
+    // Returns a bool indicating if the cursor is hover on tile
+    private bool CursorIsOnTile(float mouseXLocation, float mouseYLocation)
     {
-        rend.material.color = Color.green;
-        isHighlighted = true;
+        if ((-0.5f < mouseXLocation && mouseXLocation < 0.5) && (-0.5 < mouseYLocation && mouseYLocation < 0.5))
+        {
+            return true;
+        }
+        return false;
     }
 
-    public void UnHighlight()
+    private void LocateWalls(char layoutChar)
     {
-        isHighlighted = false;
-        rend.material.color = Color.white;
+        switch (layoutChar)
+        {
+            case 'w':
+                isWall = true;
+                canMoveDown = false;
+                canMoveLft = false;
+                canMoveUp = false;
+                canMoveRght = false;
+                break;         
+            case '-':   // - means half wall is at top of square
+                canMoveUp = false;
+                break;              
+            case '|':   // | means half wall on left side of a square
+                canMoveLft = false; 
+                break;
+            case '/':
+                canMoveLft = false;
+                canMoveUp = false;
+                break;
+            case '\\':
+                canMoveUp = false;
+                canMoveRght = false;
+                break;
+            case 'L':
+                canMoveLft = false;
+                canMoveDown = false;
+                break;
+            case '_':
+                canMoveRght = false;
+                canMoveDown = false;
+                break;
+            default:
+                return;
+        }
     }
 
-    // Creates an empty tile, sets its location, adds a box collider
-    public void Initialize(Board gameBoard, float xLocation, float yLocation, float zLocation)
-    {
-        this.xCoordinate = xLocation;
-        this.yCoordinate = yLocation;
-        this.zCoordinate = zLocation;
-        this.gameBoard = gameBoard;
-        this.gameObject.AddComponent(typeof(BoxCollider));
-        this.transform.position = new Vector3(xCoordinate, yCoordinate, zCoordinate);
+   public List<GridSquare> GetNeighbors() {
+        if (_neighbors != null)
+            return _neighbors;
+
+        _neighbors = new List<GridSquare>();
+        //@TODO need to catch outof bounds exception
+        GridSquare rightNeighb = gameBoard.tiles[column +1, row];  // column, row
+        GridSquare leftNeighb = gameBoard.tiles[column -1, row];
+        GridSquare aboveNeighb = gameBoard.tiles[column, row -1];
+        GridSquare belowNeighb = gameBoard.tiles[column, row +1];
+
+        if (this.canMoveRght && rightNeighb.canMoveLft)
+            _neighbors.Add(rightNeighb);
+
+        if (this.canMoveLft && leftNeighb.canMoveRght)
+            _neighbors.Add(leftNeighb);
+
+        if (this.canMoveDown && belowNeighb.canMoveUp)
+            _neighbors.Add(belowNeighb);
+
+        if (this.canMoveUp && aboveNeighb.canMoveDown)
+            _neighbors.Add(aboveNeighb);
+
+        return _neighbors;
+    }
+
+    public override string ToString() {
+        return column + "," + row;
     }
 }
+
 
---
+++ Assets/Utilities/BoardCreationUtility/Scripts/WallCreator.cs
@@ -0,0 +1,61 @@
+﻿//using System.IO;
+//using UnityEngine;
+
+//public class Board : MonoBehaviour
+//{
+//    private readonly int GRID_HEIGHT = 32;
+//    private readonly int GRID_WIDTH = 24;
+//    private readonly string MAP_NAME = "LatestMap";
+//    string[,] gameBoard;
+
+
+
+//    void Start()
+//    {
+//        CreateGrid();
+//    }
+
+//    void OnApplicationQuit()
+//    {
+//        string boardAsAString = "";
+//        for (int row = 0; row < GRID_HEIGHT; row++)
+//        {
+//            boardAsAString += "\n";
+//            for (int column = 0; column < GRID_WIDTH; column++)
+//            {
+//                boardAsAString += gameBoard[column, row];
+//            }
+//        }
+
+//        string fileLocation = Directory.GetCurrentDirectory() + "\\Assets\\Utilities\\BoardCreationUtility\\output\\" + MAP_NAME + ".txt";
+//        Debug.Log(fileLocation);
+//        File.WriteAllText(fileLocation, boardAsAString);
+//    }
+
+//    public void MarkSquareAsWall(int column, int row)
+//    {
+//        gameBoard[column, row] = "w";
+//    }
+
+//    public void UnMarkSquareAsWall(int column, int row)
+//    {
+//        gameBoard[column, row] = ".";
+//    }
+
+//    private void CreateGrid()
+//    {
+//        gameBoard = new string[GRID_WIDTH, GRID_HEIGHT];
+
+//        for (int row = 0; row < GRID_HEIGHT; row++)
+//        {
+//            for (int column = 0; column < GRID_WIDTH; column++)
+//            {
+//                GameObject gridSquare = Instantiate(Resources.Load("Prefabs/opaqueSquare") as GameObject);
+//                GridSquare emptyTile = gridSquare.AddComponent<GridSquare>();
+//                emptyTile.Initialize(this, column, -row, -1);
+
+//                gameBoard[column, row] = ".";
+//            }
+//        }
+//    }
+//}
