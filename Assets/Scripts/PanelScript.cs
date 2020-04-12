05b5a3e2430512ddf70546dcc503c66c5d42891c
ranjitjohn01 <38595565+ranjitjohn01@users.noreply.github.com>
2020-04-12 14:26:32 -0700

    PanelChanges and Player MouseDown added

---
+++ Assets/Scripts/PanelScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelScript : MonoBehaviour
+{
+    public GameObject panel;
+    public bool status;
+    public void DisplayPanel()
+    {
+        panel.gameObject.SetActive(true);
+        status = true;
+    }
+    public void HidePanel()
+    {
+        panel.gameObject.SetActive(false);
+        status = false;
+    }
+    public bool PanelStatus()
+    {
+        return status;
+    }
+
+    public void Move()
+    {
+        Debug.Log("Move Button Clicked");
+        HidePanel();
+    }
+    public void Attack()
+    {
+        Debug.Log("Attck Button Clicked");
+        HidePanel();
+    }
+}
